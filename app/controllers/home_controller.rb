f95e430fbf81e9cde67e417f64980b06c97dd82e
Marian Diaconu <neboduus@gmail.com>
2019-01-29 16:16:36 +0100

    just some notes about secrets (how to store API keys)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
+# Just a NOTE:
+# in order to read encrypted credentials
+# Rails.application.credentials.aws[:access_key_id]
+
+# If u want to add environments (just add them as we were doing in old secrets)
+# Rails.application.credentials[Rails.env.to_sym][:aws][:access_key_id]
+
 class HomeController < ApplicationController
   def index
   end
